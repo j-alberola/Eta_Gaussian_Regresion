ceb9d8bf36ef0eb1a98ef05782e876355dc3a565
j-alberola <josepalberolaboloix@gmail.com>
2025-04-16 11:46:24 +0200

    Delete Test_direct directory

--- Test_direct/version_original_newest_eta7/change_kappa_original.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#sed -i "s/kappa = 2/kappa = $1/" PROGRAM_bayesian_optimization_direct_deriv.py
-sed -i "s/kappa = 2/kappa = $1/" PROGRAM_bayesian_optimization_direct_deriv_test_original.py
-#sed -i "s/xi = 2/xi = $1/" PROGRAM_bayesian_optimization_direct_deriv.py
-sed -i "s/xi = 2/xi = $1/" PROGRAM_bayesian_optimization_direct_deriv_test_original.py
